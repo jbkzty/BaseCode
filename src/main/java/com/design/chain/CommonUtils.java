90b6654f937c8a8523782209c56591a859176835
季炳坤 <jibingkun791@pingan.com.cn>
2017-11-05 13:09:58 +0800

    [REV]责任链代码

---
+++ src/main/java/com/design/chain/CommonUtils.java
@@ -0,0 +1,8 @@
+package com.design.chain;
+
+/**
+ * @author spuerKun
+ * @date 17/11/3.
+ */
+public class CommonUtils {
+}
---
+++ src/main/java/com/design/chain/char2/Client.java
@@ -0,0 +1,8 @@
+package com.design.chain.char2;
+
+/**
+ * @author spuerKun
+ * @date 17/11/5.
+ */
+public class Client {
+}
